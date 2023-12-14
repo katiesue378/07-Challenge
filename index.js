95cfe517f2f6d682cf7f6e375a0189bf8779fe45
katiesue378 <133729753+katiesue378@users.noreply.github.com>
2023-12-13 22:27:19 -0700

    fixed files

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
 
-// Internal modules
-const api = require('./utils/api.js');
-const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // Inquirer prompts for userResponses
 const questions = [
